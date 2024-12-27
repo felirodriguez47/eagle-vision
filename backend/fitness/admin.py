6df238584a01d58b1859cbc61f6880c52caa1fe2
felirodriguez47 <frodri66@palermo.edu>
2024-12-27 06:16:21 -0300

    added superuser. registered models to admin site

--- backend/fitness/admin.py
+++ backend/fitness/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Exercise, Routine, Workout, Set
 # Register your models here.
+
+admin.site.register(Exercise)
+admin.site.register(Routine)
+admin.site.register(Workout)
+admin.site.register(Set)
